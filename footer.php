1859a8d7d763d65ecdb7149a40b6d3c0a71ad628
Guillaume Molter <gmolter@bgcom.ch>
2013-05-21 11:02:52 -0400

    Original Code

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * The template for displaying the footer.
+ */
+?>
+
+	<footer>
+	
+	</footer>
+	<?php wp_footer(); ?>
+
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bgcom.js"></script>
+	
+	<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-16147619-16']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+	</script>
+	
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Theme Name: BLANK
+Theme URI: http://bgcom.ch
+Description: A blank canvas theme for webdevelopers using WP
+Author: Guillaume Molter - B+G & Partners SA, Montreux, Switzerland
+Author URI: http://bgcom.ch
+Version: 1.0
+*/
+
+/* 
+ *
+ * All the function code should go into /customize :
+ * /customize/admin.php > All admin styles and tweaks code
+ * /customize/posts-taxos.php > All custom post and custom taxonomies code
+ * /customize/toolkit.php > Other wp filters and custom made functions
+ * /customize/widgets.php > Other wp filters and custom made functions
+ *
+ */
+
+require_once("customize/admin.php"); 
+require_once("customize/posts-taxos.php"); 
+require_once("customize/toolkit.php"); 
+require_once("customize/widgets.php"); 
+
+?>
