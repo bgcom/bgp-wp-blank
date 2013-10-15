21624fe0cdd28f6b62fe7e4ace27bbdd246ce9ab
Guillaume Molter <gmolter@bgcom.ch>
2013-10-15 16:00:53 -0400

    More polyvalent file naming
    Changed the some file name to be less explicit. And deleted useles
    files.

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 	<?php wp_footer(); ?>
 
 	<script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bgcom.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 	
 	<script type="text/javascript">
 
